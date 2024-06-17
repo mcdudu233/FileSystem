54a030856706cc07f8b810365d369eeef342e9fe
倪B城 <xubiaoyu040827@outlook.com>
2024-06-17 20:25:04 +0800

    增加用户类

---
+++ include/user.h
@@ -0,0 +1,26 @@
+//
+// Created by 86138 on 2024/6/17.
+//
+
+#ifndef FILESYSTEM_USER_H
+#define FILESYSTEM_USER_H
+
+#include "chrono"
+#include "ctime"
+#include "string"
+using namespace std;
+
+class user {
+private:
+    string name; // 用户名
+    string password; // 用户密码
+    bool superuser; // 超级用户
+    int uid; // 用户uid
+
+public:
+    bool changePassword(string password); // 改密码命令
+
+};
+
+
+#endif//FILESYSTEM_USER_H
---
+++ src/user.cpp
@@ -0,0 +1,10 @@
+//
+// Created by dudu234 on 2024/6/17.
+//
+
+#include "user.h"
+
+bool user::changePassword(std::string password) {
+    this->password=password;
+    return true;
+}
