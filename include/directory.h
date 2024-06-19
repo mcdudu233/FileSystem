213f0878b1b6051cf2b1f1b01513d34b153ad3ca
倪B城 <xubiaoyu040827@outlook.com>
2024-06-19 20:52:03 +0800

    增加目录类的方法

--- include/directory.h
+++ include/directory.h
@@ -44,10 +44,12 @@ public:
     bool hasMasterPrivilege_write( char masterPrivilege);     // 判断所有者是否有写入权限
     bool hasMasterPrivilege_execute( char masterPrivilege);   // 判断所有者是否有执行权限
     bool hasOtherPrivilege_read(char otherPrivilege);         //判断其他用户是否有读取权限
-    bool hasOtherPrivilege_write(char otherPrivilege);         //判断其他用户是否有写入权限
-    string getFather();                                        // 获取父目录名
-    void serialize(fstream &out) const;                        // 序列化
-    void deserialize(fstream &in);                             // 反序列化
+    bool hasOtherPrivilege_write(char otherPrivilege);        //判断其他用户是否有写入权限
+    bool hasOtherPrivilege_execute(char masterPrivilege);     //判断其他用户是否有执行权限
+    string getFather();                                       // 获取父目录名
+    void serialize(fstream &out) const;                       // 序列化
+    void deserialize(fstream &in);                            // 反序列化
+
 };
 
 // 根目录
